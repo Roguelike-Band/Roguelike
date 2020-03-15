be4bfa5da81ef3ae80e53974795dbba80bd1bfda
Alexey Trilis <aatrilis2000@gmail.com>
2020-03-15 21:48:30 +0300

    Terminate on window closing

--- roguelike/src/main/kotlin/ru/spbau/roguelike/ui/ConsoleInputReader.kt
+++ roguelike/src/main/kotlin/ru/spbau/roguelike/ui/ConsoleInputReader.kt
@@ -2,6 +2,7 @@ package ru.spbau.roguelike.ui
 
 import com.googlecode.lanterna.input.KeyType
 import ru.spbau.roguelike.controller.Turn
+import kotlin.system.exitProcess
 
 class ConsoleInputReader(private val lanterna: Lanterna) {
     fun readTurn(): Turn {
@@ -13,6 +14,7 @@ class ConsoleInputReader(private val lanterna: Lanterna) {
                 KeyType.ArrowUp    -> return Turn.MOVEMENT_UP
                 KeyType.ArrowLeft  -> return Turn.MOVEMENT_LEFT
                 KeyType.ArrowRight -> return Turn.MOVEMENT_RIGHT
+                KeyType.EOF -> exitProcess(0)
             }
         }
     }
