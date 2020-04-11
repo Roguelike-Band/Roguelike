3145ac5884681f8d333bea39d84e382cda94e2e3
Igor Smirnov <piter3343@gmail.com>
2020-04-11 22:43:38 +0300

    added conected field generation

--- roguelike/src/main/kotlin/ru/spbau/roguelike/model/field/Coordinates.kt
+++ roguelike/src/main/kotlin/ru/spbau/roguelike/model/field/Coordinates.kt
@@ -4,4 +4,4 @@ import kotlinx.serialization.Serializable
 
 /** Coordinates on a field */
 @Serializable
-class Coordinates(val row: Int, val column: Int)
+data class Coordinates(val row: Int, val column: Int)
--- roguelike/src/main/kotlin/ru/spbau/roguelike/model/field/FieldGenerator.kt
+++ roguelike/src/main/kotlin/ru/spbau/roguelike/model/field/FieldGenerator.kt
@@ -1,8 +1,10 @@
 package ru.spbau.roguelike.model.field
 
 import ru.spbau.roguelike.model.field.objects.EmptyCell
+import ru.spbau.roguelike.model.field.objects.FieldObjectType
 import ru.spbau.roguelike.model.field.objects.Wall
 import java.io.File
+import java.util.ArrayDeque
 import kotlin.random.Random
 
 /**
@@ -11,7 +13,7 @@ import kotlin.random.Random
  * Allows to load field from file or generate random field
  */
 object FieldGenerator {
-    /** Generates random field with given parameters */
+    /** Generates random connected field with given parameters */
     fun generateField(parameters: FieldGenerationParameters): Field {
         val field = Array(parameters.height) {
             Array(parameters.width) {
@@ -22,7 +24,84 @@ object FieldGenerator {
                 }
             }
         }
+        connectField(field)
         return Field(field)
+    }
+
+    private fun connectField(field: Array<Array<FieldObject>>) {
+        val colors = paintComponents(field)
+        val centralCell = Coordinates(field.size / 2, field[0].size / 2)
+        val seenColors = mutableSetOf<Int>()
+        for (row in 0..field.lastIndex) {
+            for (column in 0..field[0].lastIndex) {
+                if (colors[row][column] != 0 && colors[row][column] !in seenColors) {
+                    connectCells(field, centralCell, Coordinates(row, column))
+                    seenColors.add(colors[row][column])
+                }
+            }
+        }
+    }
+
+    private fun paintComponents(field: Array<Array<FieldObject>>): Array<IntArray> {
+        val colors = Array(field.size) { IntArray(field[0].size) { 0 } }
+        var currentComponentId = 0
+        for (row in 0..field.lastIndex) {
+            for (column in 0..field[0].lastIndex) {
+                if (field[row][column].objectType == FieldObjectType.EMPTY_CELL && colors[row][column] == 0) {
+                    paintConcreteComponent(Coordinates(row, column), colors, ++currentComponentId, field)
+                }
+            }
+        }
+        return colors
+    }
+
+    private fun paintConcreteComponent(coordinates: Coordinates,
+                                       colors: Array<IntArray>,
+                                       currentColor: Int,
+                                       field: Array<Array<FieldObject>>) {
+        val queue = ArrayDeque<Coordinates>()
+        queue.add(coordinates)
+        colors[coordinates.row][coordinates.column] = currentColor
+        while (queue.isNotEmpty()) {
+            val currentCoordinates = queue.pollFirst()
+            for (stepDirectory in listOf(Pair(1, 0), Pair(-1, 0), Pair(0, 1), Pair(0, -1))) {
+                val newCoordinates = Coordinates(currentCoordinates.row + stepDirectory.first,
+                    currentCoordinates.column + stepDirectory.second)
+
+                if (0 <= newCoordinates.row && newCoordinates.row < colors.size &&
+                    0 <= newCoordinates.column && newCoordinates.column < colors[0].size) {
+                    if (field[newCoordinates.row][newCoordinates.column].objectType == FieldObjectType.EMPTY_CELL &&
+                            colors[newCoordinates.row][newCoordinates.column] == 0) {
+                        colors[newCoordinates.row][newCoordinates.column] = currentColor
+                        queue.add(newCoordinates)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun connectCells(field: Array<Array<FieldObject>>, from: Coordinates, to: Coordinates) {
+        var currentCoordinates = from
+        while (currentCoordinates != to) {
+            field[currentCoordinates.row][currentCoordinates.column] = EmptyCell()
+            val changeColumnCoordinate = currentCoordinates.row == to.row ||
+                    (currentCoordinates.column != to.column && Random.nextBoolean())
+            currentCoordinates = if (changeColumnCoordinate) {
+                Coordinates(currentCoordinates.row,
+                    currentCoordinates.column + moveDelta(currentCoordinates.column, to.column))
+            } else {
+                Coordinates(currentCoordinates.row + moveDelta(currentCoordinates.row, to.row),
+                    currentCoordinates.column)
+            }
+        }
+    }
+
+    private fun moveDelta(from: Int, to: Int): Int {
+        return if (from > to) {
+            -1
+        } else {
+            1
+        }
     }
 
     /**
